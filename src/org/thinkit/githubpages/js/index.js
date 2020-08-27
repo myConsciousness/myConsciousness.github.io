9f4bd169c8d6ed7240cbef3330eea091d00b7400
myConsciousness <yourdream28@gmail.com>
2020-08-27 15:03:50 +0900

    update

--- src/org/thinkit/githubpages/js/index.js
+++ src/org/thinkit/githubpages/js/index.js
@@ -6,4 +6,18 @@
         $(".loader").fadeOut();
         $("#preloder").delay(200).fadeOut("slow");
     });
+
+
+    $('#bar1').barfiller({
+        barColor: "#04c2c9",
+    });
+
+    $('#bar2').barfiller({
+        barColor: "#04c2c9",
+    });
+
+    $('#bar3').barfiller({
+        barColor: "#04c2c9",
+    });
+
 })(jQuery);
