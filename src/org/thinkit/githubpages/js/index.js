03b5f89a0c422ad13515352959ea8b4f10310dca
myConsciousness <yourdream28@gmail.com>
2020-08-28 10:56:25 +0900

    update

--- src/org/thinkit/githubpages/js/index.js
+++ src/org/thinkit/githubpages/js/index.js
@@ -2,17 +2,42 @@
 
 (function($) {
 
-    $(window).on('load', function() {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
+    fadeOutPreLoader();
+    scrollSmoothly();
+    fillBars(10);
 
-
-    for (let i = 1; i < 10; i++) {
-        $('#bar' + String(i)).barfiller({
-            barColor: "#04c2c9",
+    function fadeOutPreLoader() {
+        $(window).on('load', function() {
+            $(".loader").fadeOut();
+            $("#preloder").delay(200).fadeOut("slow");
         });
     }
 
+    function scrollSmoothly() {
+        $('.page-link').click(function() {
+            var anchor = $(this).attr('destination');
+
+            // $('.link-wrap').removeClass('visible');
+            // $('nav span').removeClass('active');
+
+            // $('nav')
+            //     .find('[dest="' + anchor + '"]')
+            //     .addClass('active');
+
+            $('html, body').animate({
+                    scrollTop: $('#' + anchor).offset().top
+                },
+                400
+            );
+        });
+    }
+
+    function fillBars(count) {
+        for (let i = 1; i < count; i++) {
+            $('#bar' + String(i)).barfiller({
+                barColor: "#04c2c9",
+            });
+        }
+    }
 
 })(jQuery);
