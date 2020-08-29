ca98910642fffb4b6456f72e4843b14730f88985
myConsciousness <yourdream28@gmail.com>
2020-08-29 20:16:33 +0900

    update

--- src/org/thinkit/githubpages/js/barfiller.js
+++ src/org/thinkit/githubpages/js/barfiller.js
@@ -16,18 +16,22 @@ const SKILL_SET = {
 const createSkillBars = () => {
 
     const skillBarsObject = document.getElementById('skill-bars');
+
+    let skillSet = '';
     let count = 0;
 
-    Object.keys(SKILL_SET).forEach(function() {
-        skillBarsObject.innerHTML =
+    Object.keys(SKILL_SET).forEach(function(skill) {
+        skillSet +=
             `<div class="skill-bar-item">
-                <p>${key}</p>
+                <p>${skill}</p>
                 <div id="bar${++count}" class="barfiller">
                     <span class="tip"></span>
-                    <span class="fill" data-percentage="${value}"></span>
+                    <span class="fill" data-percentage="${SKILL_SET[skill]}"></span>
                 </div>
             </div>`
     });
+
+    skillBarsObject.innerHTML = skillSet;
 }
 
 createSkillBars();
